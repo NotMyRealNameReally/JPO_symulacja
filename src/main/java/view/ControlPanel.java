641597fe98b822a2a5d4c0d981b04d6d8e6c7d0d
NotMyRealNameReally <jeremiacz.cieslinski@gmail.com>
2021-01-15 15:43:39 +0100

    Added variable world size.

--- src/main/java/view/ControlPanel.java
+++ src/main/java/view/ControlPanel.java
@@ -3,24 +3,30 @@ package view;
 import java.awt.FlowLayout;
 import java.nio.charset.StandardCharsets;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class ControlPanel extends JPanel {
-    private JButton newWorldBtn;
-    private JButton nextTurnBtn;
+    private final JButton newWorldBtn;
+    private final JButton nextTurnBtn;
+    private final JSpinner worldSizeSpinner;
 
     public ControlPanel(ControlPanelListener listener){
         newWorldBtn = new JButton(new String("Nowy świat".getBytes(), StandardCharsets.UTF_8));
         nextTurnBtn = new JButton(new String("następna tura".getBytes(), StandardCharsets.UTF_8));
+        worldSizeSpinner = new JSpinner(new SpinnerNumberModel(20, 5, 1000, 1));
         attachBtnListeners(listener);
+
         setLayout(new FlowLayout());
         add(nextTurnBtn);
         add(newWorldBtn);
+        add(worldSizeSpinner);
     }
 
     private void attachBtnListeners(ControlPanelListener listener) {
-        newWorldBtn.addActionListener(e -> listener.createNewWorld(20, 20));
+        newWorldBtn.addActionListener(e -> {
+            int size = (int) worldSizeSpinner.getValue();
+            listener.createNewWorld(size, size);
+        });
         nextTurnBtn.addActionListener(e -> listener.nextTurn());
     }
 }
